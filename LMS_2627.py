60034fb23f710708a6f9145d38738e81f544e992
Matt Allen <matt.allen111@gmail.com>
2026-08-09 22:47:06 +0100

    updated script

--- LMS_2627.py
+++ LMS_2627.py
@@ -186,7 +186,7 @@ st.write(f"Logged in as: **{player}**")
 
 
 #fixtures = pd.read_excel(r"C:\Users\matta\OneDrive\Documents\Matt's Stuff\Footy\PL2627.xlsx")
-url = "https://github.com/Mallos17/LMS_2627/blob/main/PL2627.xlsx"
+url = "https://raw.githubusercontent.com/Mallos17/LMS_2627/main/PL2627.xlsx"
 fixtures = pd.read_excel(url)
 
 # Convert
